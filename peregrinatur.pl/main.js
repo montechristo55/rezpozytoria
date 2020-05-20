10e91ac99245875049662f602b44d1b9e5115c59
first name_ last name <monte_christo_02@poczta.onet.pl>
2020-05-20 09:24:38 +0200

    20maj, grafika needed

---
+++ peregrinatur.pl/main.js
@@ -0,0 +1,7 @@
+// alert(`Dzień dobry!`);
+const firstName = `Krzysztof`;
+const age = 39;
+
+console.log(firstName);
+console.log(age);
+console.log (`Witam, mam na imie ${firstName} i mam ${age} lat.` );
